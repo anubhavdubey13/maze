26fad1dac86b0ca2d634af2cae7a86deeb48dc18
anubhavdubey13 <anubhav.dubey.stats@gmail.com>
2023-02-28 12:56:55 +0530

    ability of draw lines on command

--- main.py
+++ main.py
@@ -21,6 +21,30 @@ class Window:
         self.__window_running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
 
-win = Window(800, 600)
-win.wait_for_close()
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, p1, p2):
+        self.__p1 = p1
+        self.__p2 = p2
+    
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.__p1.x, self.__p1.y, self.__p2.x, self.__p2.y,
+            fill=fill_color, width = 2
+        )
+        canvas.pack()
+
+def main():
+    win = Window(800, 600)
+    win.draw_line(Line(Point(10,15), Point(8, 92)), fill_color="red")
+    win.draw_line(Line(Point(15,15), Point(92, 92)), fill_color="black")
+    win.wait_for_close()
+
+main()
