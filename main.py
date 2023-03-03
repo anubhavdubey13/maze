2589db0a759bd63a4651477398769d34c7dbd7f2
anubhavdubey13 <anubhav.dubey.stats@gmail.com>
2023-03-03 19:11:59 +0530

    Solve method in Maze using Depth search

--- main.py
+++ main.py
@@ -104,7 +104,8 @@ class Cell:
         c2_x_mid, c2_y_mid = to_cell.mid_point()
         if undo:
             col = "gray"
-        col = "red"
+        else:
+            col = "red"
         self._win.draw_line(Line(Point(c1_x_mid, c1_y_mid), Point(c2_x_mid, c2_y_mid)), fill_color=col)
 
 class Maze:
@@ -200,6 +201,52 @@ class Maze:
             for j in range(self._num_rows):
                 self._cells[i][j].visited = False
 
+    def solve(self):
+        return self._solve_r(i=0, j=0)
+    
+    def _solve_r(self, i, j):
+        if self._num_cols - 1 == i:
+            if self._num_rows - 1 == j:
+                return True
+            if self._num_rows - 2 == j:
+                return True
+        if self._num_cols - 2 == i and self._num_rows - 1 == j:
+            return True
+        
+        self._animate()
+        self._cells[i][j].visited = True
+
+        while True:
+            if i == self._num_cols-1 and j == self._num_rows-1:
+                return True
+            if i > 0 and not self._cells[i][j].has_left_wall and not self._cells[i-1][j].visited:
+                self._cells[i][j].draw_move(self._cells[i-1][j])
+                if self._solve_r(i-1, j):
+                    return True
+                else:
+                    self._cells[i][j].draw_move(self._cells[i-1][j], undo=True)
+            if i < self._num_cols - 1 and not self._cells[i][j].has_right_wall and not self._cells[i+1][j].visited:
+                self._cells[i][j].draw_move(self._cells[i+1][j])
+                if self._solve_r(i+1, j):
+                    return True
+                else:
+                    self._cells[i][j].draw_move(self._cells[i+1][j], undo=True)
+            if j > 0 and not self._cells[i][j].has_top_wall and not self._cells[i][j-1].visited:
+                self._cells[i][j].draw_move(self._cells[i][j-1])
+                if self._solve_r(i, j-1):
+                    return True
+                else:
+                    self._cells[i][j].draw_move(self._cells[i][j-1], undo=True)
+            if j < self._num_rows - 1 and not self._cells[i][j].has_bottom_wall and not self._cells[i][j+1].visited:
+                self._cells[i][j].draw_move(self._cells[i][j+1])
+                if self._solve_r(i, j+1):
+                    return True
+                else:
+                    self._cells[i][j].draw_move(self._cells[i][j+1], undo=True)
+            return False
+
+
+
 def main():
     win = Window(800, 600)
     #win.draw_line(Line(Point(10,15), Point(8, 92)), fill_color="red")
@@ -210,6 +257,7 @@ def main():
     #c2.draw_cell()
     #c2.draw_move(c1)
     m = Maze(10, 20, 20, 10, 20, 20, win)
+    m.solve()
     win.wait_for_close()
 
 main()
