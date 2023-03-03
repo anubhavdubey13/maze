74c45adcf1fe61c2da5ab831bc8885e487d3b150
anubhavdubey13 <anubhav.dubey.stats@gmail.com>
2023-03-03 17:25:20 +0530

    Reset cells visited method in Maze class

--- main.py
+++ main.py
@@ -121,6 +121,8 @@ class Maze:
             random.seed(seed)
         self._create_cells()
         self._break_entrance_and_exit()
+        self._break_walls_r(0, 0)
+        self._reset_cells_visited()
 
     def _create_cells(self):
         for i in range(self._num_cols):
@@ -192,6 +194,11 @@ class Maze:
                         self._cells[i][j1].has_top_wall = False
             
             self._break_walls_r(i1, j1)
+
+    def _reset_cells_visited(self):
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._cells[i][j].visited = False
 
 def main():
     win = Window(800, 600)
